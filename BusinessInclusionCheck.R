80dd8df438b1c456357856bfd57258677c04a48f
gamaynard <galphonsemaynard@gmail.com>
2020-05-28 17:51:05 -0400

    inclues processing of non-profit data as well

--- BusinessInclusionCheck.R
+++ BusinessInclusionCheck.R
@@ -1,143 +1,225 @@
-## ---------------------------
-##
-## Script name: BusinessInclusionCheck
-##
-## Purpose of script: Checks to see which businesses were dropped from or added to the dataset
-##    between the two reports
-##
-## Author: George A. Maynard
-##
-## Date Created: 2020-05-28
-##
-## Copyright (c) George A. Maynard, 2020
-## Email: galphonsemaynard@gmail.com
-##
-## ---------------------------
-##
-## Notes:
-##   
-##
-## ---------------------------
-
-## set working directory
-
-## ---------------------------
-
-## set options
-options(scipen = 6, digits = 4) 
-## ---------------------------
-
-## load up the packages we will need:  
-library(XLConnect)
-library(stringdist)
-## ---------------------------
-
-## load up our functions into memory
-
-## ---------------------------
-
-## Read in the 2020 NAICS.com data for a list of blue businesses included in the 2020 dataset
-naics2020=read.csv("/home/george/Documents/Independent_Analytics/BlueEconomy2020/2020 Analysis/bertdayabase6262019 appended.csv")
-
-## Read in the modified 2017 data NAICS data from the previous report
-## The first sheet of the file is the provider data
-naics2017=readWorksheetFromFile(
-  file="/home/george/Documents/Independent_Analytics/BlueEconomy2020/Blue Economy_oldreport/MASTER File to be Pivoted.xlsx",
-  sheet=1,
-  startRow=1,
-  endCol=22
-)
-
-## New companies are those that exist in 2020 but not 2017
-new=subset(naics2020,toupper(naics2020$COMPANY)%in%toupper(naics2017$COMPANY)==FALSE)$COMPANY
-
-## "gone" refers  to companies that existed in 2017 but no longer exist in 2020
-gone=subset(naics2017,toupper(naics2017$COMPANY)%in%toupper(naics2020$COMPANY)==FALSE)$COMPANY
-
-## Use fuzzy matching to come up with a list of possible matches between new and gone
-## Create an empty matrix to store all results
-matches=matrix(nrow=length(new),ncol=7)
-## Create an empty matrix to store most likely results (>90%)
-likely=matrix(nrow=length(new),ncol=3)
-## For each new company, find the closest match(es) in the gone companies
-for(i in 1:length(new)){
-  matches[i,1]=new[i]
-  d=stringsim(
-    a=toupper(new[i]),
-    b=toupper(gone)
+  ## ---------------------------
+  ##
+  ## Script name: BusinessInclusionCheck
+  ##
+  ## Purpose of script: Checks to see which businesses were dropped from or added to the dataset
+  ##    between the two reports
+  ##
+  ## Author: George A. Maynard
+  ##
+  ## Date Created: 2020-05-28
+  ##
+  ## Copyright (c) George A. Maynard, 2020
+  ## Email: galphonsemaynard@gmail.com
+  ##
+  ## ---------------------------
+  ##
+  ## Notes:
+  ##   
+  ##
+  ## ---------------------------
+  
+  ## set working directory
+  
+  ## ---------------------------
+  
+  ## set options
+  options(scipen = 6, digits = 4) 
+  ## ---------------------------
+  
+  ## load up the packages we will need:  
+  library(XLConnect)
+  library(stringdist)
+  ## ---------------------------
+  
+  ## load up our functions into memory
+  
+  ## ---------------------------
+  
+  ## Read in the 2020 NAICS.com data for a list of blue businesses included in the 2020 dataset
+  naics2020=read.csv("/home/george/Documents/Independent_Analytics/BlueEconomy2020/2020 Analysis/bertdayabase6262019 appended.csv")
+  
+  ## Read in the modified 2017 data NAICS data from the previous report
+  ## The first sheet of the file is the provider data
+  naics2017=readWorksheetFromFile(
+    file="/home/george/Documents/Independent_Analytics/BlueEconomy2020/Blue Economy_oldreport/MASTER File to be Pivoted.xlsx",
+    sheet=1,
+    startRow=1,
+    endCol=22
   )
-  p=gone[which(d==max(d))]
-  if(length(p)<5){
-    matches[i,seq(2,2+length(p)-1,1)]=p
-    matches[i,7]=FALSE
-  } else {
-    matches[i,seq(2,6,1)]=p[1:5]
-    matches[i,7]=TRUE
+  
+  ## New companies are those that exist in 2020 but not 2017
+  new=subset(naics2020,toupper(naics2020$COMPANY)%in%toupper(naics2017$COMPANY)==FALSE)$COMPANY
+  
+  ## "gone" refers  to companies that existed in 2017 but no longer exist in 2020
+  gone=subset(naics2017,toupper(naics2017$COMPANY)%in%toupper(naics2020$COMPANY)==FALSE)$COMPANY
+  
+  ## Use fuzzy matching to come up with a list of possible matches between new and gone
+  ## Create an empty matrix to store all results
+  matches=matrix(nrow=length(new),ncol=7)
+  ## Create an empty matrix to store most likely results (>90%)
+  likely=matrix(nrow=length(new),ncol=3)
+  ## For each new company, find the closest match(es) in the gone companies
+  for(i in 1:length(new)){
+    matches[i,1]=new[i]
+    d=stringsim(
+      a=toupper(new[i]),
+      b=toupper(gone)
+    )
+    p=gone[which(d==max(d))]
+    if(length(p)<5){
+      matches[i,seq(2,2+length(p)-1,1)]=p
+      matches[i,7]=FALSE
+    } else {
+      matches[i,seq(2,6,1)]=p[1:5]
+      matches[i,7]=TRUE
+    }
+    likely[i,1]=new[i]
+    if(max(d)>0.9){
+      likely[i,2]=gone[which(d==max(d))][1]
+      likely[i,3]=max(d)*100
+    }
   }
-  likely[i,1]=new[i]
-  if(max(d)>0.9){
-    likely[i,2]=gone[which(d==max(d))][1]
-    likely[i,3]=max(d)*100
-  }
-}
-colnames(matches)=c("NEW","PossibleMatch1","PossibleMatch2","PossibleMatch3","PossibleMatch4",
-  "PossibleMatch5","AdditionalMatchesFound")
-colnames(likely)=c("NEW","PossibleMatch","PercentMatch")
-
-## Likely matches are only those with a 90% or greater match
-likely=subset(likely,likely[,3]>=90)
-
-## Write everything out to an Excel workbook with multiple spreadsheets
-wb=loadWorkbook(
-  "/home/george/Documents/Independent_Analytics/BlueEconomy2020/2020 Analysis/Matches.xlsx", 
-  create = TRUE
+  colnames(matches)=c("NEW","PossibleMatch1","PossibleMatch2","PossibleMatch3","PossibleMatch4",
+    "PossibleMatch5","AdditionalMatchesFound")
+  colnames(likely)=c("NEW","PossibleMatch","PercentMatch")
+  
+  ## Likely matches are only those with a 90% or greater match
+  likely=subset(likely,likely[,3]>=90)
+  
+  ## Write everything out to an Excel workbook with multiple spreadsheets
+  wb=loadWorkbook(
+    "/home/george/Documents/Independent_Analytics/BlueEconomy2020/2020 Analysis/BUS_Matches.xlsx", 
+    create = TRUE
+    )
+  
+  ## Create a worksheet in the workbook for each result object and write out the results
+  createSheet(
+    wb, 
+    name="newBusinesses"
   )
-
-## Create a worksheet in the workbook for each result object and write out the results
-createSheet(
-  wb, 
-  name="newBusinesses"
-)
-writeWorksheet(
-  wb, 
-  new, 
-  sheet="newBusinesses", 
-  startRow=1, 
-  startCol=1
-)
-createSheet(
-  wb,
-  name="gone"
-)
-writeWorksheet(
-  wb,
-  gone,
-  sheet="gone",
-  startRow=1,
-  startCol=1
-)
-createSheet(
-  wb,
-  name="LikelyMatches"
-)
-writeWorksheet(
-  wb,
-  likely,
-  sheet="LikelyMatches",
-  startRow=1,
-  startCol=1
-)
-createSheet(
-  wb,
-  name="PossibleMatches"
-)
-writeWorksheet(
-  wb,
-  matches,
-  sheet="PossibleMatches",
-  startRow=1,
-  startCol=1
-)
-## Saves the workbook to the corresponding Excel file and write the file to disk
-saveWorkbook(wb)
-
+  writeWorksheet(
+    wb, 
+    as.data.frame(new), 
+    sheet="newBusinesses", 
+    startRow=1, 
+    startCol=1
+  )
+  createSheet(
+    wb,
+    name="gone"
+  )
+  writeWorksheet(
+    wb,
+    as.data.frame(gone),
+    sheet="gone",
+    startRow=1,
+    startCol=1
+  )
+  createSheet(
+    wb,
+    name="LikelyMatches"
+  )
+  writeWorksheet(
+    wb,
+    as.data.frame(likely),
+    sheet="LikelyMatches",
+    startRow=1,
+    startCol=1
+  )
+  createSheet(
+    wb,
+    name="PossibleMatches"
+  )
+  writeWorksheet(
+    wb,
+    as.data.frame(matches),
+    sheet="PossibleMatches",
+    startRow=1,
+    startCol=1
+  )
+  ## Saves the workbook to the corresponding Excel file and write the file to disk
+  saveWorkbook(wb)
+  
+  ## Read in the non-profit list identified in the previous report
+  np2017=read.csv("/home/george/Documents/Independent_Analytics/BlueEconomy2020/2020 Analysis/blue_eo.csv")
+  ## Subset out only the blue non-profits
+  np2017=subset(np2017,np2017$BLUE=="Y")
+  ## Check to see if those non-profits exist in the 2020 NAICS data or the
+  present=subset(np2017,toupper(np2017$NAME)%in%toupper(naics2020$COMPANY))$NAME
+  missing=subset(np2017,np2017$NAME%in%present==FALSE)$NAME
+  ## Use fuzzy matching to check for other potentials following the same procedure as above
+  ## Create an empty matrix to store all results
+  matches=matrix(nrow=length(missing),ncol=7)
+  ## Create an empty matrix to store most likely results (>90%)
+  likely=matrix(nrow=length(missing),ncol=3)
+  ## For each missing non-profit, find the closest match(es) in the business data
+  for(i in 1:length(missing)){
+    matches[i,1]=missing[i]
+    d=stringsim(
+      a=toupper(missing[i]),
+      b=toupper(naics2020$COMPANY)
+    )
+    p=naics2020$COMPANY[which(d==max(d))]
+    if(length(p)<5){
+      matches[i,seq(2,2+length(p)-1,1)]=p
+      matches[i,7]=FALSE
+    } else {
+      matches[i,seq(2,6,1)]=p[1:5]
+      matches[i,7]=TRUE
+    }
+    likely[i,1]=missing[i]
+    if(max(d)>0.9){
+      likely[i,2]=naics2020$COMPANY[which(d==max(d))][1]
+      likely[i,3]=max(d)*100
+    }
+  }
+  colnames(matches)=c("Missing","PossibleMatch1","PossibleMatch2","PossibleMatch3","PossibleMatch4",
+                      "PossibleMatch5","AdditionalMatchesFound")
+  colnames(likely)=c("Missing","PossibleMatch","PercentMatch")
+  
+  ## Likely matches are only those with a 90% or greater match
+  likely=subset(likely,likely[,3]>=90)
+  
+  ## Write everything out to an Excel workbook with multiple spreadsheets
+  wb=loadWorkbook(
+    "/home/george/Documents/Independent_Analytics/BlueEconomy2020/2020 Analysis/NP_Matches.xlsx", 
+    create = TRUE
+  )
+  
+  ## Create a worksheet in the workbook for each result object and write out the results
+  createSheet(
+    wb, 
+    name="missingNPs"
+  )
+  writeWorksheet(
+    wb, 
+    as.data.frame(missing), 
+    sheet="missingNPs", 
+    startRow=1, 
+    startCol=1
+  )
+  createSheet(
+    wb,
+    name="LikelyMatches"
+  )
+  writeWorksheet(
+    wb,
+    as.data.frame(likely),
+    sheet="LikelyMatches",
+    startRow=1,
+    startCol=1
+  )
+  createSheet(
+    wb,
+    name="PossibleMatches"
+  )
+  writeWorksheet(
+    wb,
+    as.data.frame(matches),
+    sheet="PossibleMatches",
+    startRow=1,
+    startCol=1
+  )
+  ## Saves the workbook to the corresponding Excel file and write the file to disk
+  saveWorkbook(wb)
